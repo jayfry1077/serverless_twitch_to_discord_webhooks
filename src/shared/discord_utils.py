0c8e878d142722790bb00bfdd8ca85c7a6f15ba3
Jonathan Bradbury <53982942+jayfry1077@users.noreply.github.com>
2021-06-30 20:23:12 -0700

    feat: added better messaging

--- src/shared/discord_utils.py
+++ src/shared/discord_utils.py
@@ -1,8 +1,14 @@
 import requests
-import json
+from aws_lambda_powertools import Logger
+
+logger = Logger(service='discord-utils')
 
 
-def call_discord(discord_webhook, data):
-    res = requests.post(discord_webhook, json={'content': json.dumps(data)}, headers={
-                        'Content-type': 'application/json'})
-    print('Response ' + res.text)
+def call_discord(discord_webhook=str, message=str):
+
+    try:
+        res = requests.post(discord_webhook, json={'content': message}, headers={
+                            'Content-type': 'application/json'})
+        logger.info('Response ' + res.text)
+    except Exception as e:
+        logger.error(f'Error while making discord call: {e}')
--- src/stream_online.py
+++ src/stream_online.py
@@ -1,8 +1,8 @@
-import json
 import os
 from aws_lambda_powertools.utilities import parameters
 from aws_lambda_powertools import Logger
 from shared.discord_utils import call_discord
+from shared.twitch_utils import get_live_stream_info
 
 
 STAGE = os.environ.get('STAGE', 'dev')
@@ -13,4 +13,23 @@ discord_callback_url = parameters.get_parameter(
 
 def main(event, context):
 
-    call_discord(discord_callback_url, event['detail']['event'])
+    broadcaster_id = event['detail']['event']['broadcaster_user_id']
+    broadcaster_name = event['detail']['event']['broadcaster_user_name']
+    broadcaster_url_id = event['detail']['event']['broadcaster_user_login']
+
+    logger.info(
+        f'Streamer [{broadcaster_name}] went live, sending discord notification')
+
+    try:
+        live_stream_info = get_live_stream_info(broadcaster_id)
+        title = ''
+        # To handle the usecase where a streamer goes live, but we were unable to retrieve their stream title for some reason.
+        if len(live_stream_info) != 0:
+            title = live_stream_info["title"]
+
+        discord_message = f':red_circle: {broadcaster_name} is live!\n{title}\nhttps://www.twitch.tv/{broadcaster_url_id}'
+
+        call_discord(discord_callback_url, discord_message)
+
+    except Exception as e:
+        logger.error(e)
