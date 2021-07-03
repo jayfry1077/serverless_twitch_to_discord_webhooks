73c964296a2f5e8204f78a0db28a286c693e7095
Jonathan Bradbury <53982942+jayfry1077@users.noreply.github.com>
2021-07-03 13:42:19 -0700

    feat: adding guilded disabling discord for now

--- src/shared/twitch_utils.py
+++ src/shared/twitch_utils.py
@@ -12,6 +12,7 @@ CLIENT_SECRET = parameters.get_parameter(
     f'/{STAGE}/twitch/client/secret', decrypt=True)
 TWITCH_OAUTH_URL = 'https://id.twitch.tv/oauth2/token'
 TWITCH_STREAMER_URL = 'https://api.twitch.tv/helix/streams'
+STREAMER_INFO_URL = 'https://api.twitch.tv/helix/users'
 
 
 def get_bearer_token():
@@ -36,3 +37,15 @@ def get_live_stream_info(stremer_id):
     logger.info(res)
 
     return res['data']
+
+
+def get_streamer_info(streamer_login=str):
+
+    params = {'login': streamer_login}
+    headers = {'Client-ID': CLIENT_ID,
+               'Authorization': f'Bearer {get_bearer_token()}'}
+
+    res = requests.get(STREAMER_INFO_URL,
+                       params=params, headers=headers).json()
+
+    return res['data'][0]['profile_image_url']
--- src/stream_online.py
+++ src/stream_online.py
@@ -2,8 +2,9 @@ import os
 from aws_lambda_powertools.utilities import parameters
 from aws_lambda_powertools import Logger
 from shared.discord_utils import call_discord
-from shared.twitch_utils import get_live_stream_info
+from shared.twitch_utils import get_live_stream_info, get_streamer_info
 from shared.dynamo_utils import get_url
+from shared.guilded_utils import call_guilded
 
 
 STAGE = os.environ.get('STAGE', 'dev')
@@ -14,10 +15,11 @@ def main(event, context):
 
     broadcaster_id = event['detail']['event']['broadcaster_user_id']
 
-    discord_callback_url = get_url(broadcaster_id)
+    callback_url = get_url(broadcaster_id)
 
     broadcaster_name = event['detail']['event']['broadcaster_user_name']
     broadcaster_url_id = event['detail']['event']['broadcaster_user_login']
+    profile_image_url = get_streamer_info(broadcaster_url_id)
 
     logger.info(
         f'Streamer [{broadcaster_name}] went live, sending discord notification')
@@ -28,12 +30,15 @@ def main(event, context):
         game_name = 'game name not found'
         # To handle the usecase where a streamer goes live, but we were unable to retrieve their stream title for some reason.
         if len(live_stream_info) != 0:
-            title = f'\n{live_stream_info[0]["title"]}'
+            title = f'{live_stream_info[0]["title"]}'
             game_name = live_stream_info[0]['game_name']
 
-        discord_message = f'{broadcaster_name} is live - {game_name}{title}\nhttps://www.twitch.tv/{broadcaster_url_id}'
+        call_guilded(
+            callback_url, f'{broadcaster_name} is live - {game_name}', f'{title}', f'https://www.twitch.tv/{broadcaster_url_id}', profile_image_url)
 
-        call_discord(discord_callback_url, discord_message)
+        # discord_message = f'{broadcaster_name} is live - {game_name}{title}\nhttps://www.twitch.tv/{broadcaster_url_id}'
+
+        # call_discord(callback_url, discord_message)
 
     except Exception as e:
         logger.error(e)
